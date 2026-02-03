5f9c92b34df8d59f03b7ffb1f27fdad42c13c8e2
Emily <emilyckellerman@gmail.com>
2026-02-03 12:29:57 +0200

    changed alles

--- obj/Debug/net8.0/CalculatorDomainDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalculatorDomainDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c15b54f7b7e88f18e91b05e2d93c308457f0685")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d4eeef119c3b5e6321ff21676b504c48fa7f9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorDomainDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
